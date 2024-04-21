4abf0c71d622a22a08f1ac5b96e1f82279698ea2
Eugene <evgkisevgkis@gmail.com>
2024-04-21 08:35:42 +0500

    написание функции создания базы данных

--- homework-5/main.py
+++ homework-5/main.py
@@ -16,33 +16,41 @@ def main():
     create_database(params, db_name)
     print(f"БД {db_name} успешно создана")
 
-    params.update({'dbname': db_name})
-    try:
-        with psycopg2.connect(**params) as conn:
-            with conn.cursor() as cur:
-                execute_sql_script(cur, script_file)
-                print(f"БД {db_name} успешно заполнена")
-
-                create_suppliers_table(cur)
-                print("Таблица suppliers успешно создана")
-
-                suppliers = get_suppliers_data(json_file)
-                insert_suppliers_data(cur, suppliers)
-                print("Данные в suppliers успешно добавлены")
-
-                add_foreign_keys(cur, json_file)
-                print(f"FOREIGN KEY успешно добавлены")
-
-    except(Exception, psycopg2.DatabaseError) as error:
-        print(error)
-    finally:
-        if conn is not None:
-            conn.close()
+    # params.update({'dbname': db_name})
+    # try:
+    #     with psycopg2.connect(**params) as conn:
+    #         with conn.cursor() as cur:
+    #             execute_sql_script(cur, script_file)
+    #             print(f"БД {db_name} успешно заполнена")
+    #
+    #             create_suppliers_table(cur)
+    #             print("Таблица suppliers успешно создана")
+    #
+    #             suppliers = get_suppliers_data(json_file)
+    #             insert_suppliers_data(cur, suppliers)
+    #             print("Данные в suppliers успешно добавлены")
+    #
+    #             add_foreign_keys(cur, json_file)
+    #             print(f"FOREIGN KEY успешно добавлены")
+    #
+    # except(Exception, psycopg2.DatabaseError) as error:
+    #     print(error)
+    # finally:
+    #     if conn is not None:
+    #         conn.close()
 
 
 def create_database(params, db_name) -> None:
     """Создает новую базу данных."""
-    pass
+    conn = psycopg2.connect(dbname='postgres', **params)
+    conn.autocommit = True
+    cur = conn.cursor()
+
+    cur.execute(f"DROP DATABASE IF EXISTS {db_name}")
+    cur.execute(f"CREATE DATABASE {db_name}")
+
+    conn.close()
+
 
 def execute_sql_script(cur, script_file) -> None:
     """Выполняет скрипт из файла для заполнения БД данными."""
