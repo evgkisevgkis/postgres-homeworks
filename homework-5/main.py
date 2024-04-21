e769ed2eac1df98edae169b8ac69034d131703bf
Eugene <evgkisevgkis@gmail.com>
2024-04-21 08:48:05 +0500

    написание функции наполнения таблиц

--- homework-5/main.py
+++ homework-5/main.py
@@ -16,12 +16,12 @@ def main():
     create_database(params, db_name)
     print(f"БД {db_name} успешно создана")
 
-    # params.update({'dbname': db_name})
-    # try:
-    #     with psycopg2.connect(**params) as conn:
-    #         with conn.cursor() as cur:
-    #             execute_sql_script(cur, script_file)
-    #             print(f"БД {db_name} успешно заполнена")
+    params.update({'dbname': db_name})
+    try:
+        with psycopg2.connect(**params) as conn:
+            with conn.cursor() as cur:
+                execute_sql_script(cur, script_file)
+                print(f"БД {db_name} успешно заполнена")
     #
     #             create_suppliers_table(cur)
     #             print("Таблица suppliers успешно создана")
@@ -33,11 +33,11 @@ def main():
     #             add_foreign_keys(cur, json_file)
     #             print(f"FOREIGN KEY успешно добавлены")
     #
-    # except(Exception, psycopg2.DatabaseError) as error:
-    #     print(error)
-    # finally:
-    #     if conn is not None:
-    #         conn.close()
+    except(Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
 
 
 def create_database(params, db_name) -> None:
@@ -54,7 +54,9 @@ def create_database(params, db_name) -> None:
 
 def execute_sql_script(cur, script_file) -> None:
     """Выполняет скрипт из файла для заполнения БД данными."""
-
+    with open(script_file, 'r') as file:
+        data = file.read()
+    cur.execute(data)
 
 
 def create_suppliers_table(cur) -> None:
